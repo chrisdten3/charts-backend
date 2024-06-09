b83960bbe2b7c6aee949208be7b2bcadb1032d5a
chrisdten3 <123044283+chrisdten3@users.noreply.github.com>
2024-06-09 17:01:12 -0400

    changing cors headers

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from efCalc import get_portfolio_allocations
 from efCalc import get_history
 from efCalc import get_stock_data
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "methods": "*", "allow_headers": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.before_request
+def handle_options():
+    if request.method == 'OPTIONS':
+        response = app.make_response('')
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+        return response
 
 @app.route("/api/portfolio", methods=["GET"])
+@cross_origin()
 def get_portfolio():
     tickers = request.args.get("tickers")
     tickers = tickers.split(",")
@@ -20,6 +31,7 @@ def get_portfolio():
     })
 
 @app.route("/api/history", methods=["GET"])
+@cross_origin()
 def get_stock_history():
     ticker = request.args.get("ticker")
     history = get_history(ticker)
@@ -27,6 +39,7 @@ def get_stock_history():
     return history
 
 @app.route("/api/stock", methods=["GET"])
+@cross_origin()
 def get_stock():
     ticker = request.args.get("ticker")
     info = get_stock_data(ticker)
